c459dc4aa5031ce5dac0fa1bac07d3fb4bcdbac0
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-10-23 21:03:20 +0700

    p

--- tugas2.java
+++ tugas2.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
 public class tugas2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jmlKampus, jmlCabor;
+        String Kampus, cabor, atlet;
+
+        System.out.print("Input Jumlah Politeknik Yang Mendaftar : ");
+        jmlKampus = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 1; i <= jmlKampus; i++) {
+            System.out.print("Nama Politeknik ke-" + i + " : ");
+            Kampus = sc.nextLine();
+            System.out.print("Jumlah Cabor : ");
+            jmlCabor = sc.nextInt();
+            sc.nextLine();
+
+            for (int j = 1; j <= jmlCabor; j++) {
+                System.out.print("\nNama Cabor ke-" + j + " : ");
+                cabor = sc.nextLine();
+
+                for (int k = 1; k <= 5; k++) {
+                    System.out.print("Nama Atlet ke-" + k + " : ");
+                    atlet = sc.nextLine();
+                }
+            }
+            System.out.println();
+        }
+    }
 }
