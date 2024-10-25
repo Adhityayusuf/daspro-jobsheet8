70859c558fe8fac2e23c6910a70383d8b825e0a7
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-10-25 20:14:00 +0700

    p

--- RataNilai13.java
+++ RataNilai13.java
@@ -4,19 +4,32 @@ public class RataNilai13 {
         Scanner sc = new Scanner(System.in);
         int i, j, nilaiMhs, mahasiswa;
         float nilai, totalNilai, rataNilai;
+        String nama;
+
+        System.out.println("=== MENGHITUNG NILAI RATA-RATA MAHASISWA ===");
+        System.out.print("\nJumlah Mahasiswa yg di input : ");
+        mahasiswa = sc.nextInt();
+        System.out.print("Jumlah Matkul yg di input    : ");
+        nilai = sc.nextInt();
 
         i = 1;
-        while (i<=5) {
-            System.out.println("Input Nilai Mahasiswa ke " + i);
+        while (i <= mahasiswa) {
+            System.out.println("\nInput Nilai Mahasiswa ke " + i);
+            sc.nextLine();
+            System.out.print("Input Nama Mahasiswa : ");
+            nama = sc.nextLine();
+
             totalNilai = 0;
-            for (j=1;j<=5;j++) {
+            for (j = 1; j <= nilai; j++) {
                 System.out.print("Nilai ke-" + j + " = ");
                 nilaiMhs = sc.nextInt();
                 totalNilai += nilaiMhs;
             }
-            rataNilai = totalNilai / 5;
-            System.out.println("Rata-rata Nilai Mahasiswa ke " + i + " adalah " + rataNilai);
+
+            rataNilai = totalNilai / nilai;
+            System.out.println("Rata-rata Nilai Mahasiswa atas nama " + nama + " adalah " + rataNilai);
             i++;
         }
+        System.out.println("\n===  PENGINPUTAN NILAI SELESAI  ===");
     }
 }
