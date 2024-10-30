3802a6eb8959d182b55098ab56193997bbdf2a87
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-10-31 00:34:58 +0700

    p

--- ArrayRataNilai13.java
+++ ArrayRataNilai13.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ArrayRataNilai {
+public class ArrayRataNilai13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int [] nilaiMhs = new int [10];
